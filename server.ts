fcb91c9a0bff8e3fe65dc94e155b0b98978db28f
Abdulkadir Karadaş <kadirkaradas5@gmail.com>
2025-08-27 12:49:40 +0300

    feat: add server setup with dotenv configuration and Express app listener

---
+++ server.ts
@@ -0,0 +1,10 @@
+import { config } from "dotenv"
+import { app } from "./src/core/app"
+
+config();
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port: ${PORT}`);
+});
