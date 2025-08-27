463f6e9ce107fa8abfa7e1518ef17415b83c8514
Abdulkadir Karadaş <kadirkaradas5@gmail.com>
2025-08-27 12:52:23 +0300

    feat: add error handling middleware to manage server errors

--- src/core/app.ts
+++ src/core/app.ts
@@ -1,9 +1,12 @@
 import cors from 'cors';
 import express, { Application } from 'express';
 import morgan from 'morgan';
+import { errorHandler } from './middleware/errorHandler';
 
 export const app: Application = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
+
+app.use(errorHandler);
