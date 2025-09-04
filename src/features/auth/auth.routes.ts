326c720b2dd9b9c8ed682103357d9bce9984d212
Abdulkadir Karadaş <kadirkaradas5@gmail.com>
2025-09-04 15:44:36 +0300

    feat: add authRoutes file

---
+++ src/features/auth/auth.routes.ts
@@ -0,0 +1,26 @@
+import { checkAuthenticate } from "../../core/middleware/jwt/checkAuthenticate";
+import { LoginController } from "./login/login.controller";
+import { loginRequestSchema } from "./login/validationSchemes";
+import { RegisterController } from "./register/register.controller";
+import { registerRequestSchema } from "./register/validationSchemes";
+import { Router } from "express";
+import { validateRequest } from "../../core/helper/genericValidation";
+
+const router = Router();
+
+// Register Routes
+router.post(
+  "/register",
+  validateRequest(registerRequestSchema),
+  RegisterController.register
+);
+
+// Login Routes
+router.post(
+  "/login",
+  validateRequest(loginRequestSchema),
+  LoginController.login
+);
+router.get("/me", checkAuthenticate, LoginController.user);
+
+export default router;
