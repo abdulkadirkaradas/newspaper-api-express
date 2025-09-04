1e31d0f73d2aa0f6ceb29767c7cad10ed60b65ef
Abdulkadir Karadaş <kadirkaradas5@gmail.com>
2025-09-04 15:47:03 +0300

    refactor: bind routers to app. add bodyParser package call

--- src/core/app.ts
+++ src/core/app.ts
@@ -1,12 +1,19 @@
-import cors from 'cors';
-import express, { Application } from 'express';
-import morgan from 'morgan';
-import { errorHandler } from './middleware/errorHandler';
+import apiRouter from "../routes/api";
+import bodyParser from "body-parser";
+import cors from "cors";
+import express, { Application } from "express";
+import morgan from "morgan";
+import webRouter from "../routes/web";
+import { errorHandler } from "./middleware/errorHandler";
 
 export const app: Application = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
+app.use(bodyParser.json());
+
+app.use("/", webRouter);
+app.use("/api", apiRouter);
 
 app.use(errorHandler);
