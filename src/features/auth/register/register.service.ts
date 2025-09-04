e345a1b3bbbd4c5880728e3ab0414137e5c7656e
Abdulkadir Karadaş <kadirkaradas5@gmail.com>
2025-09-04 15:44:09 +0300

    feat: add service, controller and validationSchemes files for the registration operations

---
+++ src/features/auth/register/register.service.ts
@@ -0,0 +1,42 @@
+import bcrypt from "bcrypt";
+import { prisma } from "../../../core/config/database";
+
+interface Register {
+  name: string;
+  lastname: string;
+  username: string;
+  email: string;
+  password: string;
+}
+
+export class RegisterService {
+  static async register(data: Register) {
+    const { name, lastname, username, email, password } = data;
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    return prisma.user.create({
+      data: {
+        name,
+        lastname,
+        username,
+        email,
+        password: hashedPassword,
+        emailVerifiedAt: null,
+        roleId: 2,
+      },
+      select: {
+        id: true,
+        name: true,
+        lastname: true,
+        username: true,
+        email: true,
+        password: false,
+        emailVerifiedAt: false,
+        roleId: false,
+        deleted: false,
+        createdAt: true,
+        updatedAt: true,
+      },
+    });
+  }
+}
