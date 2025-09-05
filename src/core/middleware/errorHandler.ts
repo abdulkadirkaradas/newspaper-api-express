d7926179bea82886e69de287234873a62d6cffc4
Abdulkadir Karadaş <kadirkaradas5@gmail.com>
2025-09-05 13:21:39 +0300

    refactor: make errorHandler from scratch

--- src/core/middleware/errorHandler.ts
+++ src/core/middleware/errorHandler.ts
@@ -1,9 +1,45 @@
 import { Request, Response, NextFunction } from "express";
+import { Prisma } from "../../generated/prisma";
 
-export function errorHandler(err: any, req: Request, res: Response, next: NextFunction) {
-  console.error("Server Error:", err);
-  res.status(err.status || 500).json({
-    success: false,
-    message: err.message || "Internal Server Error",
-  });
+function handlePrismaError(error: any) {
+  if (error instanceof Prisma.PrismaClientKnownRequestError) {
+    switch (error.code) {
+      case "P2002":
+        return {
+          statusCode: 400,
+          message: "Duplicate value error, unique constraint violated",
+        };
+      case "P2003":
+        return { statusCode: 400, message: "Foreign key constraint violation" };
+      case "P2025":
+        return { statusCode: 404, message: "Record not found" };
+      default:
+        return { statusCode: 400, message: "Prisma request error" };
+    }
+  }
+  return null;
 }
+
+const errorHandler = (
+  err: any,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  let prismaError = handlePrismaError(err);
+  if (prismaError) {
+    return res
+      .status(prismaError.statusCode)
+      .json({ message: prismaError.message });
+  }
+
+  if (err.name === "ValidationError") {
+    return res.status(400).json({ message: err.message });
+  }
+
+  return res
+    .status(500)
+    .json({ message: "Internal server error", details: err.message });
+};
+
+export default errorHandler;
