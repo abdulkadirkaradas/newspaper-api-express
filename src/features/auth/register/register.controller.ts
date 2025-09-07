dafb102d0d2f1680605af12a2274c78e9416b160
Abdulkadir Karadaş <kadirkaradas5@gmail.com>
2025-09-07 11:09:31 +0300

    refactor: Enhance error handling in catch block

--- src/features/auth/register/register.controller.ts
+++ src/features/auth/register/register.controller.ts
@@ -1,8 +1,8 @@
-import e, { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { RegisterService } from "./register.service";
 
 export class RegisterController {
-  static async register(req: Request, res: Response) {
+  static async register(req: Request, res: Response, next: NextFunction) {
     try {
       const { name, lastname, username, email, password } = req.body;
       const user = await RegisterService.register({
@@ -14,7 +14,7 @@ export class RegisterController {
       });
       res.json(user);
     } catch (error: any) {
-      res.status(400).json({ error: error.message });
+      next(error);
     }
   }
 }
