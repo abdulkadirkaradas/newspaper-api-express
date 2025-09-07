94b9fbe4891c4fcb20cbbadfda4179a585ef5565
Abdulkadir Karadaş <kadirkaradas5@gmail.com>
2025-09-07 11:05:33 +0300

    chore: Update authentication middleware to handle refresh token

--- src/core/middleware/jwt/checkAuthenticate.ts
+++ src/core/middleware/jwt/checkAuthenticate.ts
@@ -1,11 +1,13 @@
 import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
+import { ref } from "process";
 
 interface ExtendedRequest extends Request {
   user?: any;
 }
 
-const JWT_SECRET_TOKEN: string = process.env.JWT_SECRET ?? "";
+const JWT_SECRET_ACCESS: string = process.env.JWT_SECRET_ACCESS ?? "";
+const JWT_SECRET_REFRESH: string = process.env.JWT_SECRET_REFRESH ?? "";
 
 export const checkAuthenticate = (
   req: ExtendedRequest,
@@ -19,10 +21,37 @@ export const checkAuthenticate = (
     return res.status(401).json({ message: "Unauthorized Action" });
   }
 
-  jwt.verify(token, JWT_SECRET_TOKEN, (error, user) => {
+  jwt.verify(token, JWT_SECRET_ACCESS, (error, user) => {
     if (error) {
-      return res.status(403).json({ message: "Forbidden Action" });
+      const refreshToken =
+        req.body.refreshToken ||
+        req.query.refreshToken ||
+        req.headers["x-refresh-token"];
+
+      if (!refreshToken) {
+        return res.status(403).json({ message: "Refresh token is missing" });
+      }
+
+      jwt.verify(
+        refreshToken,
+        JWT_SECRET_REFRESH,
+        (refreshError: any, refreshUser: any) => {
+          if (refreshError) {
+            return res
+              .status(403)
+              .json({ message: "Invalid or expired refresh token" });
+          }
+
+          const user = { id: refreshUser?.id };
+          const newAccessToken = jwt.sign(user, JWT_SECRET_ACCESS, {
+            expiresIn: "15m",
+          });
+
+          return res.json({ accessToken: newAccessToken });
+        }
+      );
     }
+
     req.user = user;
     next();
   });
