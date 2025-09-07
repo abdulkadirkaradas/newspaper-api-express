695e866ba57c4224f56ce3cbdceefc59fe363fc8
Abdulkadir Karadaş <kadirkaradas5@gmail.com>
2025-09-07 11:09:06 +0300

    chore: Update the register function to return access and refresh tokens

--- src/features/auth/register/register.service.ts
+++ src/features/auth/register/register.service.ts
@@ -1,5 +1,9 @@
 import bcrypt from "bcrypt";
 import { prisma } from "../../../core/config/database";
+import {
+  generateAccessToken,
+  generateRefreshToken,
+} from "../../../core/helper/jwt/generateTokens";
 
 interface Register {
   name: string;
@@ -14,7 +18,7 @@ export class RegisterService {
     const { name, lastname, username, email, password } = data;
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    return prisma.user.create({
+    const user = prisma.user.create({
       data: {
         name,
         lastname,
@@ -38,5 +42,14 @@ export class RegisterService {
         updatedAt: true,
       },
     });
+
+    const userId = (await user).id;
+    const accessToken = generateAccessToken({ id: userId });
+    const refreshToken = generateRefreshToken({ id: userId });
+
+    return {
+      accessToken,
+      refreshToken,
+    };
   }
 }
