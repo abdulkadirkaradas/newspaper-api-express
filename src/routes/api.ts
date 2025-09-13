d267e0f4ab408a7dd39ef370643968bd12154013
Abdulkadir Karadaş <kadirkaradas5@gmail.com>
2025-09-13 12:13:10 +0300

    feat: Add main route of badges feature

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,12 +1,22 @@
 import { Router } from "express";
 import announcementRouters from "../features/announcements/announcement.routes";
+import badgeRouters from "../features/badges/badge.routes";
 import authRouters from "../features/auth/auth.routes";
 import { checkAuthenticate } from "../core/middleware/jwt/checkAuthenticate";
 import { checkRole } from "../core/middleware/checkRole";
 
 const router = Router();
 
-router.use("/announcements", [checkAuthenticate, checkRole(["Admin"])], announcementRouters);
+router.use(
+  "/announcements",
+  [checkAuthenticate, checkRole(["Admin"])],
+  announcementRouters
+);
+router.use(
+  "/badges",
+  [checkAuthenticate, checkRole(["Admin", "Moderator", "Writer"])],
+  badgeRouters
+);
 router.use("/auth", authRouters);
 
 export default router;
