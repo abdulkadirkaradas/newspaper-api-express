0c96af6d0ee04b8b6fe9c0e86e38656a28c8c15a
Abdulkadir Karadaş <kadirkaradas5@gmail.com>
2025-09-13 10:35:35 +0300

    Refactor: Enhance the announcement route to use the checkAuthenticate middleware

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import announcementRouters from "../features/announcements/announcement.routes";
 import authRouters from "../features/auth/auth.routes";
+import { checkAuthenticate } from "../core/middleware/jwt/checkAuthenticate";
 
 const router = Router();
 
-router.use("/announcements", announcementRouters);
+router.use("/announcements", [checkAuthenticate], announcementRouters);
 router.use("/auth", authRouters);
 
 export default router;
