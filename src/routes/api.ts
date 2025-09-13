fd6e1e7f072df50a6884af22fa82f6145056f115
Abdulkadir Karadaş <kadirkaradas5@gmail.com>
2025-09-13 11:47:04 +0300

    chore: Add checkRole middleware to announcements routes

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -2,10 +2,11 @@ import { Router } from "express";
 import announcementRouters from "../features/announcements/announcement.routes";
 import authRouters from "../features/auth/auth.routes";
 import { checkAuthenticate } from "../core/middleware/jwt/checkAuthenticate";
+import { checkRole } from "../core/middleware/checkRole";
 
 const router = Router();
 
-router.use("/announcements", [checkAuthenticate], announcementRouters);
+router.use("/announcements", [checkAuthenticate, checkRole(["Admin"])], announcementRouters);
 router.use("/auth", authRouters);
 
 export default router;
