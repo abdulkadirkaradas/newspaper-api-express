13ec068f3445d3e12a2b4a07a7ca3c0e1ed5b0ce
Abdulkadir Karadaş <kadirkaradas5@gmail.com>
2025-10-23 15:55:27 +0300

    feat: Implement getUserInformation function to retrieve user details from the database

--- src/core/config/database.ts
+++ src/core/config/database.ts
@@ -1,3 +1,23 @@
 import { PrismaClient } from "../../generated/prisma/client";
 
 export const prisma = new PrismaClient();
+
+export async function getUserInformation(id: string) {
+  return await prisma.user.findFirst({
+    where: { id },
+    select: {
+        id: true,
+        name: true,
+        lastname: true,
+        username: true,
+        email: true,
+        password: false,
+        blocked: false,
+        emailVerifiedAt: false,
+        roleId: true,
+        deleted: false,
+        createdAt: false,
+        updatedAt: false,
+    },
+  });
+}
