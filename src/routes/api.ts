337f6fb16cdcb193987de889d53b29382185579a
Abdulkadir Karadaş <kadirkaradas5@gmail.com>
2025-10-26 13:01:24 +0300

    feat: Add user routes with authentication middleware to API

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -2,6 +2,7 @@ import announcementRouters from '../features/announcements/announcement.routes';
 import authRouters from '../features/auth/auth.routes';
 import badgeRouters from '../features/badges/badge.routes';
 import notificationRoutes from '../features/notifications/notification.routes';
+import userRoutes from '../features/users/user.routes';
 import warningRoutes from '../features/warnings/warning.routes';
 import { checkAuthenticate } from '../core/middleware/jwt/checkAuthenticate';
 import { checkRole } from '../core/middleware/checkRole';
@@ -22,5 +23,6 @@ router.use(
 router.use("/auth", authRouters);
 router.use("/notifications", [checkAuthenticate], notificationRoutes);
 router.use("/warnings", [checkAuthenticate], warningRoutes);
+router.use("/users", [checkAuthenticate], userRoutes);
 
 export default router;
