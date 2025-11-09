b48ced6ad9eda20d8b1328ecd774bcd19d1601e8
Abdulkadir Karadaş <kadirkaradas5@gmail.com>
2025-11-09 15:31:13 +0300

    feat: Add a middleware that verifies and uploads images

---
+++ src/core/middleware/fileUpload.ts
@@ -0,0 +1,25 @@
+import path from "path";
+import { createMulter } from "../helper/genericFileUpload";
+import { NextFunction, Response, RequestHandler } from "express";
+import { ExtendedRequest } from "../helper/genericTypes";
+
+export function fileUploadMiddleware(folderName: string): RequestHandler {
+  return (req: ExtendedRequest, res: Response, next: NextFunction) => {
+    const uploadDir = path.resolve(
+      process.cwd(),
+      "public",
+      "uploads",
+      folderName
+    );
+
+    const userId = req.user?.id;
+    const newsId = req.params.newsId;
+
+    const multerInstance = createMulter(uploadDir, {
+      userId: userId ?? undefined,
+      relId: newsId ?? undefined,
+    });
+
+    return multerInstance.array("image", 10)(req, res, next);
+  };
+}
