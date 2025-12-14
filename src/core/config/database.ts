22d30bacdb490434bd64ec0daefbbfae274101c1
Abdulkadir Karadaş <kadirkaradas5@gmail.com>
2025-12-14 14:04:04 +0300

    refactor: update Prisma connection to use pg-adapter

--- src/core/config/database.ts
+++ src/core/config/database.ts
@@ -1,23 +1,27 @@
+import "dotenv/config";
+import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "../../generated/prisma/client";
 
-export const prisma = new PrismaClient();
+const connectionString = `${process.env.DATABASE_URL}`;
+const adapter = new PrismaPg({ connectionString });
+export const prisma = new PrismaClient({ adapter });
 
 export async function getUserInformation(id: string) {
   return await prisma.user.findUnique({
     where: { id },
     select: {
-        id: true,
-        name: true,
-        lastname: true,
-        username: true,
-        email: true,
-        password: false,
-        blocked: false,
-        emailVerifiedAt: false,
-        roleId: true,
-        deleted: false,
-        createdAt: false,
-        updatedAt: false,
+      id: true,
+      name: true,
+      lastname: true,
+      username: true,
+      email: true,
+      password: false,
+      blocked: false,
+      emailVerifiedAt: false,
+      roleId: true,
+      deleted: false,
+      createdAt: false,
+      updatedAt: false,
     },
   });
 }
