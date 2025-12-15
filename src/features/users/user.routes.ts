e94a3352ddf53d1d160744a9449719a4b367ee91
Abdulkadir Karadaş <kadirkaradas5@gmail.com>
2025-12-15 15:02:28 +0300

    refactor: make user controller and service class-based

--- src/features/users/user.routes.ts
+++ src/features/users/user.routes.ts
@@ -1,11 +1,19 @@
 import { Router } from "express";
-import { getUser, updateRole, updateUserStatus } from "./user.controller";
+import { UserController } from "./user.controller";
 import { checkRole } from "../../core/middleware/checkRole";
 
 const router = Router();
 
-router.get("/", getUser);
-router.put("/:id/update-role", [checkRole(["Admin"])], updateRole);
-router.put("/:id/update-status", [checkRole(["Admin", "Moderator"])], updateUserStatus);
+router.get("/", UserController.getUser);
+router.put(
+  "/:id/update-role",
+  [checkRole(["Admin"])],
+  UserController.updateRole
+);
+router.put(
+  "/:id/update-status",
+  [checkRole(["Admin", "Moderator"])],
+  UserController.updateUserStatus
+);
 
 export default router;
