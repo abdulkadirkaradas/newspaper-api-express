b3d521a1ad019dd86ae70c908f5640e15f2a1fd2
Abdulkadir Karadaş <kadirkaradas5@gmail.com>
2025-12-15 15:28:16 +0300

    refactor: make warning controller and service class-based

--- src/features/warnings/warning.routes.ts
+++ src/features/warnings/warning.routes.ts
@@ -2,26 +2,28 @@ import { NotificationCreateRequestSchema } from "./validationScheme";
 import { Router } from "express";
 import { validateRequest } from "../../core/helper/genericValidation";
 
-import {
-  getAllWarnings,
-  getWarnings,
-  createWarning,
-  editWarning,
-  deleteWarning,
-} from "./warning.controller";
+import { WarningController } from "./warning.controller";
 import { checkRole } from "../../core/middleware/checkRole";
 
 const router = Router();
 
-router.get("/all", [checkRole(["Admin"])], getAllWarnings);
-router.get("/", getWarnings);
+router.get("/all", [checkRole(["Admin"])], WarningController.getAllWarnings);
+router.get("/", WarningController.getWarnings);
 router.post(
   "/",
   [checkRole(["Admin", "Moderator"])],
   validateRequest(NotificationCreateRequestSchema),
-  createWarning
+  WarningController.createWarning
 );
-router.put("/:id/update", [checkRole(["Admin", "Moderator"])], editWarning);
-router.delete("/:id/delete", [checkRole(["Admin"])], deleteWarning);
+router.put(
+  "/:id/update",
+  [checkRole(["Admin", "Moderator"])],
+  WarningController.editWarning
+);
+router.delete(
+  "/:id/delete",
+  [checkRole(["Admin"])],
+  WarningController.deleteWarning
+);
 
 export default router;
--- src/features/warnings/warning.service.ts
+++ src/features/warnings/warning.service.ts
@@ -17,55 +17,57 @@ type WarningAdminFilter = WarningDefaultFilter & {
   deleted?: boolean;
 };
 
-export async function getAllWarnings(role: number, filter: WarningAdminFilter) {
-  if (role !== 1) {
-    return { message: "Only admins can access all warnings." };
-  }
+export class WarningService {
+  static async getAllWarnings(role: number, filter: WarningAdminFilter) {
+    if (role !== 1) {
+      return { message: "Only admins can access all warnings." };
+    }
 
-  return await prisma.warning.findMany({
-    where: {
-      id: filter.id,
-      userId: filter.userId,
-      deleted: filter.deleted,
-      warningLevel: filter.warningLevel,
-    },
-  });
-}
-
-export async function getWarnings(filter: WarningDefaultFilter) {
-  if (filter.userId && filter.id) {
-    return await prisma.warning.findUnique({
-      where: { userId: filter.userId, id: filter.id, deleted: false },
-    });
-  } else if (filter.userId) {
     return await prisma.warning.findMany({
-      where: { userId: filter.userId, deleted: false },
-    });
-  } else if (filter.id) {
-    return await prisma.warning.findUnique({
-      where: { id: filter.id, deleted: false },
+      where: {
+        id: filter.id,
+        userId: filter.userId,
+        deleted: filter.deleted,
+        warningLevel: filter.warningLevel,
+      },
     });
   }
 
-  return { message: "Please provide user or warning ID!" };
-}
+  static async getWarnings(filter: WarningDefaultFilter) {
+    if (filter.userId && filter.id) {
+      return await prisma.warning.findUnique({
+        where: { userId: filter.userId, id: filter.id, deleted: false },
+      });
+    } else if (filter.userId) {
+      return await prisma.warning.findMany({
+        where: { userId: filter.userId, deleted: false },
+      });
+    } else if (filter.id) {
+      return await prisma.warning.findUnique({
+        where: { id: filter.id, deleted: false },
+      });
+    }
 
-export async function createWarning(data: Warning) {
-  return await prisma.warning.create({
-    data: data,
-  });
-}
+    return { message: "Please provide user or warning ID!" };
+  }
 
-export async function editWarning(id: string, data: Partial<Warning>) {
-  return await prisma.warning.update({
-    where: { id: id },
-    data: data,
-  });
-}
+  static async createWarning(data: Warning) {
+    return await prisma.warning.create({
+      data: data,
+    });
+  }
 
-export async function deleteWarning(id: string) {
-  return await prisma.warning.update({
-    where: { id },
-    data: { deleted: true },
-  });
+  static async editWarning(id: string, data: Partial<Warning>) {
+    return await prisma.warning.update({
+      where: { id: id },
+      data: data,
+    });
+  }
+
+  static async deleteWarning(id: string) {
+    return await prisma.warning.update({
+      where: { id },
+      data: { deleted: true },
+    });
+  }
 }
