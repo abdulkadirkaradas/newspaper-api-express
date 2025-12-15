8b230ce31eddb1a00e34c54bd54b7a12a8bd683a
Abdulkadir Karadaş <kadirkaradas5@gmail.com>
2025-12-15 14:49:29 +0300

    refactor: make badge controller and service class-based

--- src/features/badges/badge.controller.ts
+++ src/features/badges/badge.controller.ts
@@ -1,30 +1,24 @@
 import { Request, Response, NextFunction } from "express";
-import * as badgeService from "./badge.service";
+import { BadgeService } from "./badge.service";
 
-export async function getBadges(
-  req: Request,
-  res: Response,
-  next: NextFunction
-) {
-  try {
-    const { filter, id } = req.body;
-    const badges = await badgeService.getBadges(filter, id ?? null);
+export class BadgeController {
+  static async getBadges(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { filter, id } = req.body;
+      const badges = await BadgeService.getBadges(filter, id ?? null);
 
-    res.json(badges);
-  } catch (error) {
-    next(error);
+      res.json(badges);
+    } catch (error) {
+      next(error);
+    }
   }
-}
 
-export async function createBadges(
-  req: Request,
-  res: Response,
-  next: NextFunction
-) {
-  try {
-    const badge = await badgeService.createBadge(req.body);
-    res.status(201).json({ success: true, data: badge });
-  } catch (error) {
-    next(error);
+  static async createBadges(req: Request, res: Response, next: NextFunction) {
+    try {
+      const badge = await BadgeService.createBadge(req.body);
+      res.status(201).json({ success: true, data: badge });
+    } catch (error) {
+      next(error);
+    }
   }
 }
