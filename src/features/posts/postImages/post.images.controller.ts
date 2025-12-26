e3919dd9160ff9e0908639d92aee56e17ee9ff81
Abdulkadir Karadaş <kadirkaradas5@gmail.com>
2025-12-26 17:38:17 +0300

    feat(postImage): centralize messages using constants

--- src/features/posts/postImages/post.images.controller.ts
+++ src/features/posts/postImages/post.images.controller.ts
@@ -2,6 +2,7 @@ import { PostImageService } from "./post.images.service";
 import { ExtendedRequest } from "../../../core/helper/genericTypes";
 import { NextFunction, Request, Response } from "express";
 import { HTTP_STATUS } from "../../../core/helper/constants/http-status.constants";
+import { MESSAGES } from "./constants";
 
 export class PostImageController {
   static async uploadImages(
@@ -16,7 +17,7 @@ export class PostImageController {
       if (!files || files.length === 0) {
         res
           .status(HTTP_STATUS.BAD_REQUEST)
-          .json({ message: "No files uploaded" });
+          .json({ message: MESSAGES.ERROR.NO_FILES_UPLOADED });
         return;
       }
 
@@ -26,8 +27,8 @@ export class PostImageController {
       });
 
       let message = savedFiles
-        ? "Files uploaded successfully"
-        : "An error occurred";
+        ? MESSAGES.RESPONSE.UPLOADED
+        : MESSAGES.ERROR.AN_ERROR_OCCURRED;
       let status = savedFiles
         ? HTTP_STATUS.OK
         : HTTP_STATUS.INTERNAL_SERVER_ERROR;
