6d1c0208a5ec56d2acd719ee3fd56a273f0d68b6
Abdulkadir Karadaş <kadirkaradas5@gmail.com>
2026-02-02 16:31:18 +0300

    fix(genericValidation): also validate query and request parameters

--- apps/api/src/core/helper/genericValidation.ts
+++ apps/api/src/core/helper/genericValidation.ts
@@ -1,10 +1,24 @@
 import { Request, Response, NextFunction } from "express";
 import { z } from "zod";
 
-export function validateRequest(schema: z.ZodObject<any, any>) {
+type ParameterFilter = {
+  route?: boolean;
+  query?: boolean;
+};
+
+export function validateRequest(
+  schema: z.ZodObject<any, any>,
+  parameterFilter?: ParameterFilter,
+) {
   return (req: Request, res: Response, next: NextFunction) => {
     try {
-      schema.parse(req.body);
+      const value = parameterFilter?.route
+        ? req.params
+        : parameterFilter?.query
+          ? req.query
+          : req.body;
+
+      schema.parse(value);
       next();
     } catch (error) {
       next(error);
