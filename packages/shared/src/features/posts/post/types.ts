55288130d4fdbcf6cd4218a4ef01724d912ba6d9
Abdulkadir Karadaş <kadirkaradas5@gmail.com>
2026-02-10 12:37:01 +0300

    feat(shared/post): add response type for post flow

---
+++ packages/shared/src/features/posts/post/types.ts
@@ -0,0 +1,27 @@
+export interface PostFlowResponse {
+  id: number;
+  title: string;
+  content: string;
+  categoryId: string;
+  score: {
+    score: number;
+    upvotes: number;
+    downvotes: number;
+  };
+  createdAt: string;
+  author: {
+    id: string;
+    name: string;
+    lastname: string;
+    username: string;
+  };
+  images: {
+    id: string;
+    url: string;
+  }[];
+  category: {
+    id: string;
+    name: string;
+  };
+  counterPosts: PostFlowResponse[];
+}
