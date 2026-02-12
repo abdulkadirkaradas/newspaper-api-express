8b74ce6c663f31c8c5ffc8d8652dec245b33b8a1
Abdulkadir Karadaş <kadirkaradas5@gmail.com>
2026-02-12 13:55:22 +0300

    feat(api/app): enable static route for serving files

--- apps/api/src/core/app.ts
+++ apps/api/src/core/app.ts
@@ -4,21 +4,22 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import express, { Application } from "express";
 import morgan from "morgan";
+import path from "path";
 import webRouter from "@/routes/web";
 import errorHandler from "./middleware/errorHandler";
 
 export const app: Application = express();
 
 app.use(cors({
-  origin: process.env.API_ORIGIN || "http://localhost:3001",
+  origin: process.env.WEB_ORIGIN || "http://localhost:3001",
   credentials: true
 }));
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(cookieParser());
 app.use(bodyParser.json());
-
+// Serving static files
+app.use("/public", express.static(path.join(process.cwd(), "public")));
 app.use("/", webRouter);
 app.use("/api", apiRouter);
-
 app.use(errorHandler);
